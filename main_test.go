df781bebea2f7e09088ae57e7844e2afbad5c904
En Zhou <ezhouen@vendasta.com>
2021-08-13 23:57:31 +0200

    Fix test

--- main_test.go
+++ main_test.go
@@ -95,7 +95,7 @@ func TestCreateProduct(t *testing.T) {
 	}
 
 	// the id is compared to 1.0 because JSON unmarshaling converts numbers to floats, when the target is a map[string]interface{}
-	if m["id"] != 1.0 {
+	if m["ID"] != 1.0 {
 		t.Errorf("Expected product ID to be '1', Got '%v'", m["id"])
 	}
 }
